ebc590ee4830678d05d034ec06b97df36711c216
pengyi <ryanpoy@gmail.com>
2021-08-10 20:19:03 +0800

    refactoring unitests

--- utils/utils_test.go
+++ utils/utils_test.go
@@ -22,79 +22,57 @@ func TestHashCode(t *testing.T) {
 }
 
 func TestNumberOfLeadingZerosForInt(t *testing.T) {
-	f, err := os.Open("test_files/numberOfLeadingZerosForInt_10w.txt") // 用Caffeine的Java版本生成10w数据
-	if err != nil {
-		defer f.Close()
-	}
 	assert := assert.New(t)
-	reader := bufio.NewReader(f)
-	for ; ; {
-		line, ferr := reader.ReadString('\n')
-		if ferr != nil && ferr == io.EOF {
-			break
-		}
-		line = strings.TrimSpace(line)
-		vs := strings.Split(line, ",")
-
-		num, _ := strconv.Atoi(vs[0])
-		expected, _ := strconv.Atoi(vs[1])
+	numbers, expecteds := readLines("test_files/numberOfLeadingZerosForInt_10w.txt")
+	length := len(numbers)
+	for i := 0; i < length; i++ {
+		num, expected := numbers[i], expecteds[i]
 		relt := NumberOfLeadingZerosForInt(num)
 		assert.Equal(expected, relt, fmt.Sprintf("NumberOfLeadingZerosForInt(%d)=%d, 现在=%d", num, expected, relt))
 	}
 }
 
 func TestCeilingPowerOfTwoForInt(t *testing.T) {
-	f, err := os.Open("test_files/ceilingPowerOfTwoForInt_10w.txt") // 用Caffeine的Java版本生成10w数据
-	if err != nil {
-		defer f.Close()
-	}
 	assert := assert.New(t)
-	reader := bufio.NewReader(f)
-	for ; ; {
-		line, ferr := reader.ReadString('\n')
-		if ferr != nil && ferr == io.EOF {
-			break
-		}
-		line = strings.TrimSpace(line)
-		vs := strings.Split(line, ",")
-
-		num, _ := strconv.Atoi(vs[0])
-		expected, _ := strconv.Atoi(vs[1])
+	numbers, expecteds := readLines("test_files/ceilingPowerOfTwoForInt_10w.txt")
+	length := len(numbers)
+	for i := 0; i < length; i++ {
+		num, expected := numbers[i], expecteds[i]
 		relt := CeilingPowerOfTwoForInt(num)
 		assert.Equal(expected, relt, fmt.Sprintf("CeilingPowerOfTwoForInt(%d)=%d, 现在=%d", num, expected, relt))
 	}
 }
 
-
 func TestNumberOfLeadingZerosForLong(t *testing.T) {
-	f, err := os.Open("test_files/numberOfLeadingZerosForLong_10w.txt") // 用Caffeine的Java版本生成10w数据
-	if err != nil {
-		defer f.Close()
-	}
 	assert := assert.New(t)
-	reader := bufio.NewReader(f)
-	for ; ; {
-		line, ferr := reader.ReadString('\n')
-		if ferr != nil && ferr == io.EOF {
-			break
-		}
-		line = strings.TrimSpace(line)
-		vs := strings.Split(line, ",")
-
-		num, _ := strconv.Atoi(vs[0])
-		expected, _ := strconv.Atoi(vs[1])
+	numbers, expecteds := readLines("test_files/numberOfLeadingZerosForLong_10w.txt")
+	length := len(numbers)
+	for i := 0; i < length; i++ {
+		num, expected := numbers[i], expecteds[i]
 		relt := NumberOfLeadingZerosForLong(int64(num))
 		assert.Equal(expected, relt, fmt.Sprintf("NumberOfLeadingZerosForLong(%d)=%d, 现在=%d", num, expected, relt))
 	}
 }
 
 func TestCeilingPowerOfTwoForLong(t *testing.T) {
-	f, err := os.Open("test_files/ceilingPowerOfTwoForLong_10w.txt") // 用Caffeine的Java版本生成10w数据
+	assert := assert.New(t)
+	numbers, expecteds := readLines("test_files/ceilingPowerOfTwoForLong_10w.txt")
+	length := len(numbers)
+	for i := 0; i < length; i++ {
+		num, expected := numbers[i], expecteds[i]
+		relt := CeilingPowerOfTwoForLong(int64(num))
+		assert.Equal(int64(expected), relt, fmt.Sprintf("CeilingPowerOfTwoForLong(%d)=%d, 现在=%d", num, expected, relt))
+	}
+}
+
+func readLines(fname string) ([]int, []int) {
+	f, err := os.Open(fname) // 用Caffeine的Java版本生成10w数据
 	if err != nil {
 		defer f.Close()
 	}
-	assert := assert.New(t)
 	reader := bufio.NewReader(f)
+	numbers, expecteds := make([]int, 0), make([]int, 0)
+
 	for ; ; {
 		line, ferr := reader.ReadString('\n')
 		if ferr != nil && ferr == io.EOF {
@@ -105,7 +83,9 @@ func TestCeilingPowerOfTwoForLong(t *testing.T) {
 
 		num, _ := strconv.Atoi(vs[0])
 		expected, _ := strconv.Atoi(vs[1])
-		relt := CeilingPowerOfTwoForLong(int64(num))
-		assert.Equal(int64(expected), relt, fmt.Sprintf("CeilingPowerOfTwoForLong(%d)=%d, 现在=%d", num, expected, relt))
+
+		numbers = append(numbers, num)
+		expecteds = append(expecteds, expected)
 	}
+	return numbers, expecteds
 }
