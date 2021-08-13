461cf6318c2c0590599c2197becbf727bbffdecc
pengyi <ryanpoy@gmail.com>
2021-08-13 12:53:15 +0800

    重定义目录结构

--- cache/internal/lru.go
+++ cache/internal/lru.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 )
 
+// 顾名思义：LRU cache
 type LRU struct {
 	cap int
 	q   *list.List // doubly link q
@@ -20,17 +21,21 @@ func NewLRU(cap int) LRU {
 	}
 }
 
+// 使用的长度，即：里面有多少个元素
 func (lru *LRU) Len() int {
 	return lru.q.Len()
 }
 
+// 最大容量
 func (lru *LRU) Capacity() int {
 	return lru.cap
 }
 
-// 添加一个node到LRU
+// 往LRU里面添加内容
 // 永远添加到head
 //
+// @param: key 要添加的内容的key
+// @param: value 要添加的内容
 func (lru *LRU) Add(key string, value interface{}) *LRU {
 	pNewNode := NewNode(key, value)
 	if pElement, ok := lru.kv[key]; ok { // 存在，则找到queue的位置，挪动到最前面
@@ -46,6 +51,8 @@ func (lru *LRU) Add(key string, value interface{}) *LRU {
 	return lru
 }
 
+// 通过key查找内容
+// 如果找到返回内容，如果没有找到则有error
 func (lru *LRU) Get(key string) (interface{}, error) {
 	if pElement, ok := lru.kv[key]; ok { // 存在，则找到queue的位置，挪动到最前面
 		pNode := (pElement.Value).(*Node)
--- cache/internal/node.go
+++ cache/internal/node.go
@@ -20,7 +20,6 @@ func (n *Node) Value() interface{} {
 	return n.value
 }
 
-
 func (n *Node) Equals(n2 *Node) bool {
 	return n.key == n2.key && n.value == n2.value
 }
