c3983781a9b7d8662e5417b7d23fbe30acee887a
pengyi <ryanpoy@gmail.com>
2021-08-13 13:59:31 +0800

    refactoring

---
+++ cache/internal/lru.go
@@ -0,0 +1,100 @@
+package internal
+
+import (
+	"container/list"
+	"errors"
+	"fmt"
+)
+
+// 顾名思义：LRU。里面封装了一个map和一个list
+// 注意：这个LinkedHashMap是能存放的数据容量取决于cap
+type LRU struct {
+	cap     int
+	queue   *list.List // doubly link queue
+	hashmap map[string]*list.Element
+}
+
+func NewLRU(cap int) LRU {
+	return LRU{
+		cap:     cap,
+		queue:   list.New(),
+		hashmap: make(map[string]*list.Element, cap),
+	}
+}
+
+func (lru *LRU) IsFull() bool {
+	return lru.Len() >= lru.cap
+}
+
+// 使用的长度，即：里面有多少个元素
+func (lru *LRU) Len() int {
+	return lru.queue.Len()
+}
+
+// 最大容量
+func (lru *LRU) Capacity() int {
+	return lru.cap
+}
+
+// 重新分配最大容量
+func (lru *LRU) ReCapacity(cap int) *LRU {
+	if lru.cap < cap { // 当前容量比目标容量小，才需要重新分配
+		lru.cap = cap
+	}
+	return lru
+}
+
+// 往LRU里面添加内容
+// 永远添加到head
+//
+// @param: key 要添加的内容的key
+// @param: value 要添加的内容
+func (lru *LRU) Add(key string, value interface{}) *LRU {
+	pNewNode := NewNode(key, value)
+	if pElement, ok := lru.hashmap[key]; ok { // 存在，则找到queue的位置，挪动到最前面
+		pOldNode := (pElement.Value).(*Node)
+		if !pOldNode.Equals(pNewNode) {
+			pOldNode.UpdateWith(pNewNode)
+		}
+		lru.queue.MoveToFront(pElement)
+	} else if !lru.IsFull() { // 不存在，且空间没有满
+		pElement := lru.queue.PushFront(pNewNode)
+		lru.hashmap[key] = pElement
+	} else { // 不存在，空间也满了
+		lru.Eliminate()
+		pElement := lru.queue.PushFront(pNewNode)
+		lru.hashmap[key] = pElement
+	}
+	return lru
+}
+
+// 通过key查找内容
+// 如果找到返回内容，如果没有找到则有error
+func (lru *LRU) Get(key string) (interface{}, error) {
+	if pElement, ok := lru.hashmap[key]; ok { // 存在，则找到queue的位置，挪动到最前面
+		pNode := (pElement.Value).(*Node)
+		return pNode.Value(), nil
+	}
+	return nil, errors.New(fmt.Sprintf("『%s』does not exist", key))
+}
+
+// 删除一个key
+// 如果key存在，删除，并且返回，如果key不存在，返回nil
+func (lru *LRU) Remove(key string) interface{} {
+	if pElement, ok := lru.hashmap[key]; ok { // 存在，则找到queue的位置，挪动到最前面
+		lru.queue.Remove(pElement)
+		delete(lru.hashmap, key)
+
+		pNode := (pElement.Value).(*Node)
+		return pNode.Value()
+	}
+	return nil
+}
+
+// 自动淘汰，本质上就是淘汰最近最少使用的
+func (lru *LRU) Eliminate() interface{} {
+	if element := lru.queue.Back(); element != nil {
+		lru.queue.Remove(element)
+	}
+	return nil
+}
---
+++ cache/internal/lru_test.go
@@ -0,0 +1,118 @@
+package internal
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestInitial(t *testing.T) {
+	assert := assert.New(t)
+	lru := NewLRU(10)
+	assert.Equal(0, lru.Len())
+	assert.Equal(10, lru.Capacity())
+}
+
+func TestAddOnce(t *testing.T) {
+	assert := assert.New(t)
+	lru := NewLRU(10)
+	lru.Add("id_123", 123)
+	assert.Equal(1, lru.Len())
+	assert.Equal(10, lru.Capacity())
+}
+
+func TestAddTwice(t *testing.T) {
+	assert := assert.New(t)
+	lru := NewLRU(10)
+	lru.Add("id_123", 123)
+	lru.Add("id_123", 123)
+	assert.Equal(1, lru.Len())
+	assert.Equal(10, lru.Capacity())
+}
+
+func TestAddMany(t *testing.T) {
+	assert := assert.New(t)
+	lru := NewLRU(10)
+	lru.Add("id_123", 123)
+	lru.Add("id_123", 123)
+	lru.Add("id_456", 456)
+	lru.Add("id_789", 789)
+	assert.Equal(3, lru.Len())
+	assert.Equal(10, lru.Capacity())
+}
+
+func TestGetWhenNotExist(t *testing.T) {
+	assert := assert.New(t)
+	lru := NewLRU(10)
+	_, err := lru.Get("id_123")
+	assert.Equal("『id_123』does not exist", err.Error())
+}
+
+func TestGet(t *testing.T) {
+	assert := assert.New(t)
+	lru := NewLRU(10)
+	lru.Add("id_123", 123)
+	value, _ := lru.Get("id_123")
+	assert.Equal(123, value)
+}
+
+func TestUpdateWhenAndAExistKeyButDifferentValue(t *testing.T) {
+	assert := assert.New(t)
+	lru := NewLRU(10)
+	lru.Add("id_123", 123)
+	lru.Add("id_123", 456)
+	value, _ := lru.Get("id_123")
+	assert.Equal(456, value)
+}
+
+func TestRemoveAndGetNilIfDoesNotExist(t *testing.T) {
+	assert := assert.New(t)
+	lru := NewLRU(10)
+	assert.Equal(nil, lru.Remove("id_123"))
+}
+
+func TestRemove(t *testing.T) {
+	assert := assert.New(t)
+	lru := NewLRU(10)
+	lru.Add("id_123", 123)
+	assert.Equal(123, lru.Remove("id_123"))
+
+	_, err := lru.Get("id_123")
+	assert.Equal(fmt.Sprintf("『%s』does not exist", "id_123"), err.Error())
+}
+
+
+func TestFull(t *testing.T) {
+	assert := assert.New(t)
+	lru := NewLRU(3)
+	lru.Add("id_123", 123)
+	lru.Add("id_456", 456)
+	lru.Add("id_789", 789)
+	assert.Equal(true, lru.IsFull())
+}
+
+func TestAddWillBeEliminatedAutomaticWhenCapacityIsFull(t *testing.T) {
+	assert := assert.New(t)
+	lru := NewLRU(3)
+
+	lru.Add("id_123", 123)
+	lru.Add("id_456", 456)
+	lru.Add("id_789", 789)
+	lru.Add("id_abc", "abc")
+	assert.Equal(true, lru.IsFull())
+	assert.Equal(3, lru.Len())
+
+	v, _ := lru.Get("id_456")
+	assert.Equal(456, v)
+
+	v, _ = lru.Get("id_789")
+	assert.Equal(789, v)
+
+	v, _ = lru.Get("id_abc")
+	assert.Equal("abc", v)
+
+	_, v = lru.Get("id_123")
+	assert.Equal(nil, v)
+}
+
+
