018f3949b2d8429f31887e30b218315a0077f838
pengyi <ryanpoy@gmail.com>
2021-08-18 14:53:48 +0800

    all tests ok

--- cache/cache_test.go
+++ cache/cache_test.go
@@ -1,13 +1,19 @@
 package cache
 
+import (
+	"github.com/stretchr/testify/assert"
+	"goffeine/cache/internal/node"
+	"testing"
+)
+
 func newCache(n int) Cache {
 	return New(n)
 }
 
-//func TestInitial(t *testing.T) {
-//	assert := assert.New(t)
-//	cache := newCache(10)
-//	assert.Equal(10, cache.Capacity())
-//	assert.Equal(0, cache.Len())
-//	assert.Equal(false, cache.Contains(node.New("abc", 1)))
-//}
+func TestInitial(t *testing.T) {
+	assert := assert.New(t)
+	cache := newCache(10)
+	assert.Equal(10, cache.Capacity())
+	assert.Equal(0, cache.Len())
+	assert.Equal(false, cache.Contains(node.New("abc", 1)))
+}
