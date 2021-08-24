af4a8394cb07a8a0b7ab58fe69efc0d7b0aa96d4
pengyi <ryanpoy@gmail.com>
2021-08-24 22:11:51 +0800

    rename cache2.go to local_cache.go

--- cache/local_cache.go
+++ cache/local_cache.go
@@ -48,7 +48,8 @@ func (c *LocalCache) put(pNewNode *node.Node) {
 		c.hashmap.Store(pNewNode.Key, pNewNode)
 		c.putToWindowQueue(pNewNode)
 		c.sketch.Increment(pNewNode)
-		// 如果node的权重大于windowq的最大权重，push到windowq的first，否则push到windowq的last
+
+
 		// 如果window的当前权重大于window最大权重，挪动window的first，放到probation的last，直到window的当前权重小于等于window的最大权重。到此：window的当前权重已经收缩到合理值了。
 		// loop：如果cache的当前权重超出最大权重，进行淘汰：
 		//   如果probation的 victim(first) 和 candidate(last) 进行对比，按照FrequencyCandidate 和 FrequencyVictim 和 随机数 一起来判断淘汰 Victim 或者 Candidate。到此：Cache的当前权重已经收缩到合理值了。
--- cache/local_cache_test.go
+++ cache/local_cache_test.go
@@ -2,6 +2,7 @@ package cache
 
 import (
 	"github.com/stretchr/testify/assert"
+	"goffeine/cache/internal/node"
 	"testing"
 )
 
@@ -16,8 +17,8 @@ func TestPutANodeInWindowQueueLastWhenTheNodeDoesNotExistAndNodeWeightLessThanWi
 	pNode, ok := cache.windowQ.Last()
 	assert.Equal(false, ok)
 
-	cache.Put("key_1", 1, 10) // 执行完后，windowQ里面有内容了
-	cache.Put("key_2", 2, 5)
+	cache.putToWindowQueue(node.NewWithWeight("key_1", 1, 10)) // 执行完后，windowQ里面有内容了
+	cache.putToWindowQueue(node.NewWithWeight("key_2", 2, 5))
 
 	pNode, _ = cache.windowQ.Last()
 	assert.Equal("key_2", pNode.Key)
@@ -30,8 +31,8 @@ func TestPutANodeInWindowQueueFirstWhenTheNodeDoesNotExistAndNodeWeightEqualsWin
 	pNode, ok := cache.windowQ.First()
 	assert.Equal(false, ok)
 
-	cache.Put("key_1", 1, 10) // 执行完后，windowQ里面有内容了
-	cache.Put("key_2", 2, 20)
+	cache.putToWindowQueue(node.NewWithWeight("key_1", 1, 10)) // 执行完后，windowQ里面有内容了
+	cache.putToWindowQueue(node.NewWithWeight("key_2", 2, 20))
 
 	pNode, _ = cache.windowQ.First()
 	assert.Equal("key_2", pNode.Key)
@@ -44,8 +45,8 @@ func TestPutANodeInWindowQueueFirstWhenTheNodeDoesNotExistAndNodeWeightGreatWind
 	pNode, ok := cache.windowQ.First()
 	assert.Equal(false, ok)
 
-	cache.Put("key_1", 1, 10) // 执行完后，windowQ里面有内容了
-	cache.Put("key_2", 2, 20) //
+	cache.putToWindowQueue(node.NewWithWeight("key_1", 1, 10)) // 执行完后，windowQ里面有内容了
+	cache.putToWindowQueue(node.NewWithWeight("key_2", 2, 20)) //
 
 	pNode, _ = cache.windowQ.First()
 	assert.Equal("key_2", pNode.Key)
