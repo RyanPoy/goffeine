9e58f6efa14ac2bff1d1818bd47cf2fc085bcc4b
pengyi <ryanpoy@gmail.com>
2021-08-24 23:31:21 +0800

    add more test

--- cache/local_cache_test.go
+++ cache/local_cache_test.go
@@ -108,17 +108,15 @@ func TestEvictFromProbation(t *testing.T) {
 	assert.Equal(false, ok)
 }
 
-//
-//func TestEvictFromProbationShouldDoNothingWhenWindowQueueWeightLessThanMaxWeight(t *testing.T) {
-//	assert := assert.New(t)
-//	cache := newLocalCache(100, 20, 60)
-//	cache.putToWindowQueue(node.NewWithWeight("key_1", 1, 10)) //
-//	cache.putToWindowQueue(node.NewWithWeight("key_2", 2, 10)) // 到这里，windowQ的Weight超过了MaxWeight了
-//
-//	_, ok := cache.probationQ.First()
-//	assert.Equal(false, ok)
-//
-//	cache.evictFromWindow() // 不会做任何事情
-//	_, ok = cache.probationQ.First()
-//	assert.Equal(false, ok)
-//}
+func TestEvictFromProbationShouldDoNothingWhenWindowQueueWeightLessThanMaxWeight(t *testing.T) {
+	assert := assert.New(t)
+	cache := newLocalCache(100, 20, 60)
+	cache.putToWindowQueue(node.NewWithWeight("key_1", 1, 10)) //
+	cache.putToWindowQueue(node.NewWithWeight("key_2", 2, 30)) // 到这里，windowQ的Weight超过了MaxWeight了
+	cache.putToWindowQueue(node.NewWithWeight("key_3", 3, 10)) //
+	cache.putToWindowQueue(node.NewWithWeight("key_4", 4, 15)) //
+
+	cache.evictFromWindow() // 这里应该依次淘汰key_2，key_1, key_3 到 probation
+	cache.evictFromProbation() // 这里不会淘汰任何node
+	assert.Equal(50, cache.probationQ.Weight())
+}
